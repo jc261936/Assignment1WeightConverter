f5faa8c2669347c93b8f266b55580fdca1c72efb
jc261936 <james.campbell1@my.jcu.edu.au>
2021-03-25 01:09:13 +1000

    Get rid of redundancy try adding button function

--- app/src/main/java/au/edu/jcu/cp3406/assignment1/MainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/assignment1/MainActivity.java
@@ -1,10 +1,10 @@
 package au.edu.jcu.cp3406.assignment1;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //https://developer.android.com/guide/topics/ui/controls/spinner
         // Where i took/learned the code for the Spinner
-        Spinner fromSpinner = (Spinner) findViewById(R.id.convert_from);
-        Spinner toSpinner = (Spinner) findViewById(R.id.convert_to);
+        Spinner fromSpinner = findViewById(R.id.convert_from);
+        Spinner toSpinner = findViewById(R.id.convert_to);
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.units, android.R.layout.simple_spinner_item);
@@ -27,28 +27,27 @@ public class MainActivity extends AppCompatActivity {
         fromSpinner.setAdapter(adapter);
         toSpinner.setAdapter(adapter);
     }
+
     //Getting the Button to work and to use the math we made in Converter.java
     public void convert(View view) {
         Spinner fromSpinner, toSpinner;
         EditText fromText, toText;
 
-        fromSpinner = (Spinner) findViewById(R.id.convert_from);
-        toSpinner = (Spinner) findViewById(R.id.convert_to);
-        fromText = (EditText) findViewById(R.id.text_From);
-        toText = (EditText) findViewById(R.id.text_To);
+        fromSpinner = findViewById(R.id.convert_from);
+        toSpinner = findViewById(R.id.convert_to);
+        fromText = findViewById(R.id.text_From);
+        toText = findViewById(R.id.text_To);
 
 
         String fromString = (String) fromSpinner.getSelectedItem();
         String toString = (String) toSpinner.getSelectedItem();
         double input = Double.parseDouble(fromText.getText().toString());
 
-//        *THIS IS NOT RELEVANT, JUST PLAYING WITH IT*
-//        Weight fromUnit = Converter.Unit.fromString(fromString);
-//        Weight toUnit = Converter.Unit.fromString(toString);
-//
-//
-//        Converter converter = new Converter(fromUnit, toUnit);
-//        double result = converter.convert(input);
-//        toEditText.setText(String.valueOf(result));
+        Weight fromUnit = Weight.valueOf(fromString);
+        Weight toUnit = Weight.valueOf(toString);
+
+        Converter converter = new Converter(fromUnit, toUnit);
+        double result = converter.convert(input);
+        toText.setText(String.valueOf(result));
     }
 }
